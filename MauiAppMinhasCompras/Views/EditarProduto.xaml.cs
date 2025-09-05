0861c084ec6fdabce8863ea74b56a7c30a28c05b
Jhhhadev <jhaded777@gmail.com>
2025-09-05 13:02:24 -0300

    remover e selcionar itens da lista

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,9 +1,36 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
-public partial class NewPage1 : ContentPage
+public partial class EditarProduto : ContentPage
 {
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Update(produto_anexado);
+            await DisplayAlert("Sucesso!", "Registro atualizado", "OK");
+            await Navigation.PopAsync();
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
